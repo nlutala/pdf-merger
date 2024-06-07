87ab69258cf395c07fb87efb26c5fcb6c5c67dca
nlutala <nathanlutala@outlook.com>
2024-06-07 18:12:44 +0100

    Update test_pdf_merger.py with renamed method names starting with test

--- Tests/test_pdf_merger.py
+++ Tests/test_pdf_merger.py
@@ -6,19 +6,19 @@ import unittest
 from pdf_merger import PDFMerger
 
 class TestPDFMerger(unittest.TestCase):
-    def check_valueerror_is_raised_if_number_of_pdfs_is_zero(self):
-        assert 1 == 1
-
-    def check_valueerror_is_raised_if_number_of_pdfs_is_one(self):
+    def test_valueerror_is_raised_if_number_of_pdfs_is_zero(self):
         pass
 
-    def check_name_of_merge_pdf_if_no_filename_is_given(self):
+    def test_valueerror_is_raised_if_number_of_pdfs_is_one(self):
         pass
 
-    def check_name_of_merge_pdf_if_filename_is_given(self):
+    def test_name_of_merge_pdf_if_no_filename_is_given(self):
         pass
 
-    def check_number_of_pages_for_the_merged_pdf(self):
+    def test_name_of_merge_pdf_if_filename_is_given(self):
+        pass
+
+    def test_number_of_pages_for_the_merged_pdf(self):
         pass
 
     # A thought I had:
