fc917d4f71c6f2511b3d736cb46024040dac3fb6
nlutala <nathanlutala@outlook.com>
2024-06-08 15:51:11 +0100

    Update test_pdf_merger.py to have implemented tests and add example_files to run these tests on

--- Tests/test_pdf_merger.py
+++ Tests/test_pdf_merger.py
@@ -2,24 +2,42 @@
 Tests for the pdf_merger script
 '''
 
+import os
 import unittest
 from pdf_merger import PDFMerger
+from pypdf import PdfReader
 
 class TestPDFMerger(unittest.TestCase):
     def test_valueerror_is_raised_if_number_of_pdfs_is_zero(self):
-        pass
+        pdfs = []
+        with self.assertRaises(ValueError):
+            merger = PDFMerger(pdfs)
 
     def test_valueerror_is_raised_if_number_of_pdfs_is_one(self):
-        pass
+        pdfs = ["example_file_1.pdf"]
+        with self.assertRaises(ValueError):
+            merger = PDFMerger(pdfs)
 
     def test_name_of_merge_pdf_if_no_filename_is_given(self):
-        pass
+        pdfs = ["example_file_1.pdf", "example_file_2.pdf"]
+        merger = PDFMerger(pdfs)
+        merger.merge_pdfs()
+        assert "merged-file.pdf" in os.listdir()
+        os.remove("merged-file.pdf")
 
     def test_name_of_merge_pdf_if_filename_is_given(self):
-        pass
+        pdfs = ["example_file_1.pdf", "example_file_2.pdf"]
+        merger = PDFMerger(pdfs, "merged_example_file.pdf")
+        merger.merge_pdfs()
+        assert "merged_example_file.pdf" in os.listdir()
+        os.remove("merged_example_file.pdf")
 
     def test_number_of_pages_for_the_merged_pdf(self):
-        pass
-
-    # A thought I had:
-    # Put merge pdfs in another folder to avoid overwriting files!
+        pdfs = ["example_file_1.pdf", "example_file_2.pdf"]
+        merger = PDFMerger(pdfs, "merged_example_file.pdf")
+        merger.merge_pdfs()
+        file1_num_pages = PdfReader("example_file_1.pdf").get_num_pages()
+        file2_num_pages = PdfReader("example_file_2.pdf").get_num_pages()
+        merged_file_num_pages = PdfReader("merged_example_file.pdf").get_num_pages()
+        assert file1_num_pages + file2_num_pages == merged_file_num_pages
+        os.remove("merged_example_file.pdf")
