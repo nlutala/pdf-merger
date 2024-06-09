60fdaeb2d75cc6e6cc71e614a8b6f8e144a5f552
nlutala <nathanlutala@outlook.com>
2024-06-09 17:03:07 +0100

    Update test_pdf_merger.py to reflect change in implementation i.e. check directory of new file and original files

--- Tests/test_pdf_merger.py
+++ Tests/test_pdf_merger.py
@@ -18,36 +18,38 @@ class TestPDFMerger(unittest.TestCase):
         with self.assertRaises(ValueError):
             merger = PDFMerger(pdfs)
             
-    '''
-    A test that just refuses to work when it should do
-    I'll look into this a bit later... Or never
     def test_name_of_merge_pdf_if_no_filename_is_given(self):
         base_dir = os.path.dirname(__file__)
         pdfs = [os.path.join(base_dir, "example_file_1.pdf"), os.path.join(base_dir, "example_file_2.pdf")]
         merger = PDFMerger(pdfs)
-        output_file = os.path.join(base_dir, "merged_file.pdf")
-        merger.merge_pdfs()
+        output_file = os.path.join(base_dir, "merged-files", "merged_file.pdf")
+        merger.merge_pdfs("")
         assert os.path.isfile(output_file)
         os.remove(output_file)
-    '''
+        os.rmdir(os.path.join(base_dir, "merged-files"))
+        os.rmdir(os.path.join(base_dir, "original-files"))
         
     def test_name_of_merge_pdf_if_filename_is_given(self):
         base_dir = os.path.dirname(__file__)
         pdfs = [os.path.join(base_dir, "example_file_1.pdf"), os.path.join(base_dir, "example_file_2.pdf")]
         merger = PDFMerger(pdfs)
-        output_file = os.path.join(base_dir, "merged_example_file.pdf")
+        output_file = os.path.join(base_dir, "merged-files", "merged_example_file.pdf")
         merger.merge_pdfs(output_file)
         assert os.path.isfile(output_file)
         os.remove(output_file)
+        os.rmdir(os.path.join(base_dir, "merged-files"))
+        os.rmdir(os.path.join(base_dir, "original-files"))
 
     def test_number_of_pages_for_the_merged_pdf(self):
         base_dir = os.path.dirname(__file__)
         pdfs = [os.path.join(base_dir, "example_file_1.pdf"), os.path.join(base_dir, "example_file_2.pdf")]
         merger = PDFMerger(pdfs)
-        output_file = os.path.join(base_dir, "merged_new_example_file.pdf")
+        output_file = os.path.join(base_dir, "merged-files", "merged_new_example_file.pdf")
         merger.merge_pdfs(output_file)
         file1_num_pages = PdfReader(pdfs[0]).get_num_pages()
         file2_num_pages = PdfReader(pdfs[1]).get_num_pages()
         merged_file_num_pages = PdfReader(output_file).get_num_pages()
         assert file1_num_pages + file2_num_pages == merged_file_num_pages
         os.remove(output_file)
+        os.rmdir(os.path.join(base_dir, "merged-files"))
+        os.rmdir(os.path.join(base_dir, "original-files"))
